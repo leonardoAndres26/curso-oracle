a1d5629bb35ea36e850edf72451db5108831fae7
Academico <Academico@T08KETIPC15A202.instructores.senati.local>
2024-02-29 12:57:53 -0500

    ejercicio 01

---
+++ Ejercicio01.sql
@@ -0,0 +1,7 @@
+--REalizar un Backup de una base de datos
+
+--Listar a todos los customers que iniciem su id con la letra 'A'. Guardas el archivo como "ejercicio01.sql" Y lo subes a tu respositorio.
+
+Select * from Customers
+
+Select * from Customers Where CustomerID like 'A%'
